73c20ec827ced6f060c9800f0161be1fc03d6ae0
Alvin Haruna <harunaalvin69@gmail.com>
2022-08-04 15:16:20 +0100

    refactored code for getRandomCard function

--- src/black.js
+++ src/black.js
@@ -8,15 +8,6 @@ if ("serviceWorker" in navigator) {
     })
 }
 
-// let player = {
-//     name: "Per",
-//     chips: 200,
-//     eatMe: function() {
-//         console.log("Howdy");
-//     }
-// }
-
-// player.eatMe()
 
 let cards = []
 let sum = 0
@@ -28,17 +19,13 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 
-// playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
-    if (randomNumber > 10) {
-        return 10
-    } else if (randomNumber === 1) {
-        return 11
-    } else {
-        return randomNumber
-    }
+    
+    return randomNumber > 10 ? 10 
+            : randomNumber === 1 ? 11 
+            : randomNumber;
 }
 
 function startGame() {
